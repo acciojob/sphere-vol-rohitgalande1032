e2091f795bca87da8e187da589302e50157b6044
Rohit Galande <80148975+rohitgalande1032@users.noreply.github.com>
2025-01-20 08:37:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,19 +3,17 @@ function volume_sphere() {
 	let input1 = document.getElementById("radius");
 	let volume = document.getElementById("volume");
 
-	let r = parseInt(input1);
+	let r = parseInt(input1.value);
 
-	if(isNaN(r) || radius<0 ){
+	if(isNaN(r) || r<0 ){
 		volume.value = "NaN";
 		return;
 	}
 
-	const v = (4/3) * Math.pi * Math.pow(r, 3);
+	let v = (4/3) * Math.PI * Math.pow(r, 3);
 	volume.value = v.toFixed(4);
 } 
 
-let button = document.getElementById("submit");
-
-button.addEventListener('click', volume_sphere);
+document.getElementById("submit").addEventListener('click', volume_sphere)
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
